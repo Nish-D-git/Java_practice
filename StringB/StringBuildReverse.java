5c685fb1012cedb163d9e58150d170db599de0b7
Nish-D-git <nish.deshmukh465@gmail.com>
2026-01-20 11:23:48 +0530

    reverse in maunal way with 2 builder

--- StringB/StringBuildReverse.java
+++ StringB/StringBuildReverse.java
@@ -16,8 +16,23 @@ public class StringBuildReverse {
         //charAt
         System.out.print("Enter a index, less than "+name.length()+" index number: ");
         int specElem = sc.nextInt();
-        System.out.println(name.charAt(specElem));        
+        System.out.println(name.charAt(specElem));    
         
+        //reverse the string
+        // for(int i=name.length(); i<=0; i--){
+        //     System.out.println(name);
+        // }        
+
+        //using reverse functio
+        sb.reverse();
+        System.out.println(sb); //String() dont have reverse() funt; we only use reverse() with StringBuilder();
+        
+        //reverse using manual way //so it be as it was
+        StringBuilder sb2 = new StringBuilder();
+        for(int i=sb.length() - 1; i>=0; i--){
+            sb2.append(sb.charAt(i));
+        }
+        System.out.print("reverse string manual way: "+sb2);
     }
     
 }
