02337805c0c6ea5f7fee4285bb19bd97dec17e9a
Nish-D-git <nish.deshmukh465@gmail.com>
2026-01-20 10:23:12 +0530

    charAt

---
+++ StringB/StringB1.java
@@ -0,0 +1,13 @@
+package StringB;
+
+import java.util.*;
+public class StringB1 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Nishant");
+        System.out.println(sb);
+
+        //print a char
+        System.out.println(sb.charAt(0));        
+    }
+    
+}
