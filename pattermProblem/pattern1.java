147a5a0c0b4977ec51a625b04e4d24aff136f71c
Nish-D-git <nish.deshmukh465@gmail.com>
2026-01-21 23:26:42 +0530

    some experiment

--- pattermProblem/pattern1.java
+++ pattermProblem/pattern1.java
@@ -1,18 +1,21 @@
+import java.util.Scanner;
+
 public class pattern1 {
     public static void main(String[] args) {
-        
-        for(int i=0; i<=5; i++){
-            for(int j=0; j<=5; j++){
-                if (i==1||i==3||i==5) {
-                    System.out.print(i);
-                }else{
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=n; j++){
+                
+                if(i==0||i==n||j==n/2||i==n/2||j==0||j==n){
                     System.out.print("*");
-                }                
+                }else{
+                    System.out.print(" ");
+                }
             }
-            
-            System.out.println("");
-        }
-        
+            System.out.println();
+        }                
         
     }
     
