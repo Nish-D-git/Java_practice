409fd87028a0c52ba9bdad8a459027d319dd994e
Nish-D-git <nish.deshmukh465@gmail.com>
2026-01-21 23:08:01 +0530

    hollow rec/ row number

--- pattermProblem/pattern1.java
+++ pattermProblem/pattern1.java
@@ -2,9 +2,15 @@ public class pattern1 {
     public static void main(String[] args) {
         
         for(int i=0; i<=5; i++){
-            for(int j=0; j<=6; j++){
-                System.out.print("*");
+            for(int j=0; j<=5; j++){
+
+                if(i==0 || j==0 || i==5 || j==5){
+                    System.out.print(i);
+                }else{
+                    System.out.print(" ");
+                }
             }
+            
             System.out.println("");
         }
         
